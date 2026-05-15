173d53ac1409cb9dd961e6d19a5325084bea9e9c
poz <poz@poz.pet>
2026-05-15 06:21:18 +0200

    configuration.nix: fix nim typo

--- configuration.nix
+++ configuration.nix
@@ -117,7 +117,7 @@ isMaximal: {
 
       # Nim LSP is broken on Darwin and therefore
       # should be disabled by default. Users may still enable
-      # `vim.languages.vim` to enable it, this does not restrict
+      # `vim.languages.nim` to enable it, this does not restrict
       # that.
       # See: <https://github.com/PMunch/nimlsp/issues/178#issue-2128106096>
       nim.enable = false;
